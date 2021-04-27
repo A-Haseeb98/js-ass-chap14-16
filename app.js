347b4d58d5149733898b427739b8aa9eeb64f131
A-Haseeb98 <76472496+A-Haseeb98@users.noreply.github.com>
2021-04-27 08:59:48 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,36 @@
+var colors = ['yello', 'orange', 'purple','golden']
+//a
+// var addColor = prompt('what color add beginiing')
+// colors.unshift(addColor)
+// document.write(colors)
+
+//b
+// var addColor = prompt('what color add end')
+// colors.push(addColor)
+// document.write(colors)
+
+//c
+// colors.unshift('ads','adfdsf')
+// document.write(colors)
+
+//d
+// colors.push('ads','adfdsf')
+// document.write(colors)
+
+//e
+// colors.pop()
+// document.write(colors)
+
+//f
+// var num = +prompt('which index you want to add color')
+// var color = prompt("which color")
+// colors.splice(num,0,color)
+// document.write(colors)
+
+//g
+// var num = +prompt('which index you want to delete color')
+// var delNum = +prompt("how many colors you want to delete")
+// colors.splice(num,delNum)
+// document.write(colors)
+
+
